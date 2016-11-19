5c4ec9ac498aa8b82d48e6770787e9fd48a34a9e
Piotr Król <piotr.krol@3mdeb.com>
2016-11-19 01:31:17 +0100

    scripts: add ssh auth sock to container
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- scripts/apu2_fw_rel.sh
+++ scripts/apu2_fw_rel.sh
@@ -2,6 +2,7 @@
 
 docker run --rm -it \
 -v ${PWD}/apu2/apu2-documentation:/apu2-docs \
+-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
 -v ${PWD}/apu2/coreboot:/coreboot \
 -v ${PWD}/apu2/memtest86plus:/memtest86plus \
 -v ${PWD}/apu2/ipxe:/ipxe \

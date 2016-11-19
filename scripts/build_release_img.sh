9f7dd533ab582b3b415fd30683d1733f310b3145
Piotr Król <piotr.krol@3mdeb.com>
2016-11-19 02:08:32 +0100

    scripts: build components and final image
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- scripts/build_release_img.sh
+++ scripts/build_release_img.sh
@@ -3,8 +3,13 @@
 IPXE_PATH=/ipxe
 APU2_PATH=/apu2-docs
 CB_PATH=/coreboot
+SBO_PATH=$CB_PATH/payloads/pcengines/sortbootorder
+MEMTEST=/memtest86plus
 CBFSTOOL=./build/cbfstool
 
+#
+# iPXE
+#
 if [ ! -d $IPXE_PATH ]; then
   echo "ERROR: $PXE_PATH doesn't exist"
 else
@@ -14,17 +19,56 @@ fi
 
 make bin/8086157b.rom EMBED=$APU2_PATH/ipxe/menu.ipxe
 
+#
+# coreboot
+#
 cd $CB_PATH
-make crossgcc-i386 CPUS=8
-cp configs/pcengines.apu2.4.0.1.config .config
-make CPUS=8
 
-if [ -f  $CBFSTOOL ]; then
-  $CBFSTOOL ./build/coreboot.rom add -f $IPXE_PATH/src/bin/8086157b.rom -n genroms/pxe.rom -t raw
-  if [ $? != 0 ]; then
-    exit
-  fi
-  $CBFSTOOL ./build/coreboot.rom print
-else
-  echo "ERROR: $CBFSTOOL doesn't exist please build coreboot first"
+if [ ! -d $CB_PATH/util/crossgcc/xgcc ]; then
+  make crossgcc-i386 CPUS=$(nproc)
 fi
+
+cp configs/pcengines.apu2.4.0.1.config .config
+make CPUS=$(nproc)
+
+#
+# memtest86plus
+#
+
+if [ ! -d $MEMTEST ]; then
+  echo "ERROR: $MEMTEST doesn't exist"
+else
+  cd $MEMTEST
+  make clean
+fi
+
+make
+
+#
+# sortbootorder
+#
+
+if [ ! -d $SBO_PATH ]; then
+  echo "ERROR: $SBO_PATH doesn't exist"
+else
+  cd $SBO_PATH
+  make clean
+fi
+
+cd $CB_PATH/payloads/libpayload
+make defconfig
+make
+make install
+cd $SBO_PATH
+make
+
+cd $CB_PATH
+
+$CBFSTOOL $CB_PATH/build/coreboot.rom remove -n genroms/pxe.rom
+$CBFSTOOL $CB_PATH/build/coreboot.rom remove -n img/setup
+$CBFSTOOL $CB_PATH/build/coreboot.rom add -f /ipxe/src/bin/8086157b.rom -n genroms/pxe.rom -t raw
+$CBFSTOOL $CB_PATH/build/coreboot.rom add-payload -f $CB_PATH/payloads/pcengines/sortbootorder/sortbootorder.elf -n img/setup -t payload
+$CBFSTOOL $CB_PATH/build/coreboot.rom remove -n img/memtest
+$CBFSTOOL $CB_PATH/build/coreboot.rom add-payload -f /memtest86plus/memtest -n img/memtest - payload
+
+$CBFSTOOL $CB_PATH/build/coreboot.rom print $CB_PATH/build/coreboot.rom

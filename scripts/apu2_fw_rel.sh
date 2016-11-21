eb98cb165fada2cadca4b67e1ecb4a1222df055e
Piotr Król <piotr.krol@3mdeb.com>
2016-11-21 13:34:27 +0100

    scrips: fix path do apu2-documentation
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- scripts/apu2_fw_rel.sh
+++ scripts/apu2_fw_rel.sh
@@ -8,7 +8,7 @@ if [ "$1" == "build-ml" ] || [ "$1" == "flash-ml" ] || [ "$1" == "custom-ml" ]; 
   fi
   docker run --rm -it \
   -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
-  -v ${PWD}/../apu2-documentation:/apu2-docs \
+  -v ${PWD}/apu2/apu2-documentation:/apu2-docs \
   -v ${PWD}/apu2/coreboot-ml:/coreboot \
   -v ${PWD}/apu2/sortbootorder:/coreboot/payloads/pcengines/sortbootorder \
   pcengines/apu2 /bin/bash -c "/apu2-docs/scripts/build_release_img.sh $*"

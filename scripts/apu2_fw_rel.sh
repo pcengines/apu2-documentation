2af9e9dfee7e23f81ed851bf26f4f1cda39b45e8
Piotr Król <piotr.krol@3mdeb.com>
2016-11-21 17:43:40 +0100

    scripts: sortbootorder is included in pcengines mainline
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- scripts/apu2_fw_rel.sh
+++ scripts/apu2_fw_rel.sh
@@ -10,7 +10,6 @@ if [ "$1" == "build-ml" ] || [ "$1" == "flash-ml" ] || [ "$1" == "custom-ml" ]; 
   -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
   -v ${PWD}/apu2/apu2-documentation:/apu2-docs \
   -v ${PWD}/apu2/coreboot-ml:/coreboot \
-  -v ${PWD}/apu2/sortbootorder:/coreboot/payloads/pcengines/sortbootorder \
   pcengines/apu2 /bin/bash -c "/apu2-docs/scripts/build_release_img.sh $*"
 else
   if [ ! -d ${PWD}/apu2/coreboot-pcengines ];then

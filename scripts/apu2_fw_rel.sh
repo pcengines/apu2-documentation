bbe0a1966935d2df953aee9fc26240766e85ef68
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-01-16 12:54:09 +0100

    scripts: update the scripts for the new release manifests repo usage
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

--- scripts/apu2_fw_rel.sh
+++ scripts/apu2_fw_rel.sh
@@ -3,9 +3,9 @@
 echo $*
 
 if [ "$1" == "build-ml" ] || [ "$1" == "flash-ml" ] || [ "$1" == "custom-ml" ]; then
-  if [ ! -d ${PWD}/apu2/coreboot-ml ];then
-    git clone https://review.coreboot.org/coreboot.git ${PWD}/apu2/coreboot-ml
-    cd ${PWD}/apu2/coreboot-ml
+  if [ ! -d ${PWD}/apu2/coreboot ];then
+    git clone https://review.coreboot.org/coreboot.git ${PWD}/apu2/coreboot
+    cd ${PWD}/apu2/coreboot
     git submodule update --init --checkout
     git remote add pcengines http://github.com/pcengines/coreboot.git
     git fetch pcengines
@@ -14,19 +14,14 @@ if [ "$1" == "build-ml" ] || [ "$1" == "flash-ml" ] || [ "$1" == "custom-ml" ]; 
   fi
   docker run --rm -it \
   -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
-  -v ${PWD}/apu2/apu2-documentation:/apu2-documentation \
-  -v ${PWD}/apu2/coreboot-ml:/coreboot \
-  pcengines/apu2 /bin/bash -c "/apu2-documentation/scripts/build_release_img.sh $*"
+  -v ${PWD}:/release \
+  pcengines/apu2 /bin/bash -c "/release/apu2/apu2-documentation/scripts/build_release_img.sh $*"
 else
-  if [ ! -d ${PWD}/apu2/coreboot-pcengines ];then
-    git clone git@github.com:pcengines/coreboot.git ${PWD}/apu2/coreboot-pcengines
+  if [ ! -d ${PWD}/apu2/coreboot ];then
+    git clone git@github.com:pcengines/coreboot.git ${PWD}/apu2/coreboot
   fi
   docker run --rm -it \
   -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
-  -v ${PWD}/apu2/apu2-documentation:/apu2-documentation \
-  -v ${PWD}/apu2/coreboot-pcengines:/coreboot \
-  -v ${PWD}/apu2/memtest86plus:/memtest86plus \
-  -v ${PWD}/apu2/ipxe:/ipxe \
-  -v ${PWD}/apu2/sortbootorder:/coreboot/payloads/pcengines/sortbootorder \
-  pcengines/apu2 /bin/bash -c "/apu2-documentation/scripts/build_release_img.sh $*"
+  -v ${PWD}:/release \
+  pcengines/apu2 /bin/bash -c "/release/apu2/apu2-documentation/scripts/build_release_img.sh $*"
 fi
